b6d7b24e91db9f52036f875dfb319e4ceaed3ba9
stringiest <lucyjstringer@gmail.com>
2021-01-15 11:49:33 +0000

    added test Standard items: it should decrease sellIn by 1 per day

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -11,12 +11,21 @@ describe("Gilded Rose", function() {
     ];
     const gildedRose = new Shop(items);
 
-    it("should decrease the quality by 1 per day ", function() {
+    it("should decrease the quality by 1 per day", function() {
       for (let day = 0; day < 3; day++) {
         gildedRose.updateQuality();
       }
       expect(items[0].quality).toEqual(17);
       expect(items[1].quality).toEqual(4);
     });
+
+    it("should decrease the sellIn by 1 per day", function() {
+      for (let day = 0; day < 2; day++) {
+        gildedRose.updateQuality();
+      }
+      expect(items[0].sellIn).toEqual(5);
+      expect(items[1].sellIn).toEqual(0);
+    });
+
   });
 });
